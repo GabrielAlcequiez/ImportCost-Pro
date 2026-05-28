76e3919560d3801fe54737381fcccfb8bd53d3e7
Gabriel Alcequiez <gxbbab@gmail.com>
2026-05-28 10:32:40 -0400

    fix: add ByVolume method to enum ApportionmentMethod

--- ImportCostPro.Database/Entities/Enums/ApportionmentMethod.cs
+++ ImportCostPro.Database/Entities/Enums/ApportionmentMethod.cs
@@ -4,6 +4,7 @@ namespace ImportCostPro.Database.Entities.Enums
     {
         ByValue = 1,    // Method to apportion costs based on the value of the items
         ByWeight = 2,   // Method to apportion costs based on the weight of the items
-        ByQuantity = 3, // Method to apportion costs based on the quantity of the items
+        ByVolume = 3,  // Method to apportion cost based on the volume of the items
+        ByQuantity = 4, // Method to apportion costs based on the quantity of the items
     }
 }
