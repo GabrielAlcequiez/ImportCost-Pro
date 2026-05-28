53aaf2575c3c81b3ffdbfa1ab05e2eac1e57621b
Gabriel Alcequiez <gxbbab@gmail.com>
2026-05-28 07:00:17 -0400

    refactor: improve validation logic for TaxConfiguration percentages

--- ImportCostPro.Database/Entities/TaxConfiguration.cs
+++ ImportCostPro.Database/Entities/TaxConfiguration.cs
@@ -8,15 +8,27 @@ public class TaxConfiguration
     private TaxConfiguration(){}
     public TaxConfiguration(decimal generalItbisPercentage, decimal customsServiceFeePercentage)
     {
+        ValidatePercentages(generalItbisPercentage, customsServiceFeePercentage);
+
         Id = Guid.NewGuid();
         GeneralItbisPercentage = generalItbisPercentage;
         CustomsServiceFeePercentage = customsServiceFeePercentage;
     }
 
-
     public void Update(decimal generalItbis, decimal customsServiceFee)
     {
+        ValidatePercentages(generalItbis, customsServiceFee);
+
         GeneralItbisPercentage = generalItbis;
         CustomsServiceFeePercentage = customsServiceFee;
+    }
+
+    private void ValidatePercentages(decimal itbis, decimal customsFee)
+    {
+        if (itbis < 0 || itbis > 100)
+            throw new ArgumentException("El porcentaje general de ITBIS debe estar entre 0 y 100.");
+            
+        if (customsFee < 0 || customsFee > 100)
+            throw new ArgumentException("El porcentaje de la tasa de servicio aduanal debe estar entre 0 y 100.");
     }
 }
