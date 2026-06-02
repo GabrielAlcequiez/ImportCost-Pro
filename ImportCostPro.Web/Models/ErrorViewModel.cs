37240c8ac0ae739cbd20c4f1f589e4a7f769ff25
Gabriel Alcequiez <gxbbab@gmail.com>
2026-06-02 15:37:07 -0400

    refactor: remove unused ErrorViewModel and related views, update layout links

--- ImportCostPro.Web/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ImportCostPro.Web.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
